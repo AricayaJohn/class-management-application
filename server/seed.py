cc74749b99b2b2ef66f5b8356b7ccfa0321e7f94
John <jaricaya@gmail.com>
2025-03-21 16:28:13 +0800

    professor model

--- server/seed.py
+++ server/seed.py
@@ -10,6 +10,20 @@ from faker import Faker
 from app import app
 from models import db
 
+class Professor(db.Model, SerializerMixin):
+    __tablename__ = "professor"
+    serialize_rules = ('_password_hash', '-semesters.professor',) 
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    name = db.Column(db.String, nullable=False)
+    department = db.Column(db.String, nullable=False)
+    office_location = db.Column(db.String, nullable=True)
+    _password_hash = db.Column(db.String, nullable=False)
+
+    semester = db.relationship('Semester', back_populates='professor', cascade='all, delete-orphan')
+
+
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
