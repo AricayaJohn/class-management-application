9d5e256e07120e4784e228c27cfcef485d974959
John <jaricaya@gmail.com>
2025-03-23 16:36:24 +0800

    added formik logic for loginpage

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -10,8 +10,29 @@ function LoginPage() {
     const { login } = useContext(UserContext);
     const navigate = useNavigate();
 
+    //validation will appear if field is empty
     const validationSchema = yup.object({
         username: yup.string().required("Username is required"),
         password: yup.string().required("passowrd is required")
     });
+
+    //Setting up Formik for login page
+    const formik = useFormik({
+        initialValues: {
+            username: "",
+            password: "",
+        },
+        validationSchema: validationSchema,
+        onsubmit: (values) => {
+            login(values)
+             .then(() => {
+                navigate("/welcome");
+             })
+             .catch((error) => {
+                alert("Invalid credentials")
+             });
+        },
+    });
+
+    
 }
