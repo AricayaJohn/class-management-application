58f7757029843b542c35870054a3f9c017ab8aea
John <jaricaya@gmail.com>
2025-03-24 14:06:02 +0800

    Login Page format

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -33,6 +33,35 @@ function LoginPage() {
              });
         },
     });
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={formik.handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                     type="text"
+                     id="username"
+                     name="username"
+                     onChange={formik.handleChange}
+                     value={formik.values.username}
+                    />
+                    {formik.errors.username && <p>{formik.errors.username}</p>}
+                </div>
+                <div>
+                    <label htmlFor="password">Passwords:</label>
+                    <input
+                     type="password"
+                     id="password"
+                     name="password"
+                     onChange={formik.handleChange}
+                     value={formik.values.password}
+                    />
+                    {formik.errors.password && <p>{formik.errors.passoword}</p>}
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
 
-    
 }
