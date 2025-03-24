ac76ae6f3f8406cb348f3a2f29e7fc3320145b54
John <jaricaya@gmail.com>
2025-03-25 00:57:04 +0800

    added signup page in app.py

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { UserProvider } from "./Context";
 import LoginPage from "./LoginPage";
+import SignUpPage from "./SignUpPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LoginPage />}/>
+          <Route path="/signup" element={<SignUpPage />} />
         </Routes>
       </Router>
     </UserProvider>
