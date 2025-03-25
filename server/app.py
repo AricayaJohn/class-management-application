12de4ab488c243c035cccd0a375d17552ce364b1
John <jaricaya@gmail.com>
2025-03-26 03:56:14 +0800

    added semester and semesterClasses api

--- server/app.py
+++ server/app.py
@@ -65,17 +65,60 @@ class Professors(Resource):
         except Exception as e:
             return {'errors': str(e)}, 500
 
-class logout(Resource):
+class Logout(Resource):
     @login_required
     def post(self):
         logout_user()
-        return{'message': 'Logged out successfully!'}, 200
+        return {'message': 'Logged out successfully!'}, 200
 
+class Semesters(Resource):
+    def get(self):
+        semesters = db.session.query(Semester).filter_by(professor_id=current_user.id).all()
+        semesters_data = [semester.to_dict(rules=('-professor.semesters', '-classes.semester',)) for semester in semesters]
+        return make_response(semesters_data, 200)
+
+    def post(self):
+        json = request.get_json()
+        try:
+            new_semester = Semester(
+                name_year=json['name_year'],
+                professor_id=current_user.id
+            )
+            db.session.add(new_semester)
+            db.session.commit()
+            return make_response(new_semester.to_dict(), 201)
+        except IntegrityError:
+            db.session.rollback()
+            return{'errors': 'Semester already exists'}, 400
+        except Exception as e:
+            return {'errors': str(e)}, 500
+
+    def delete(self, semester_id):
+        semester = db.session.get(Semester, semester_id)
+        if not semester:
+            return {'message': 'Semester not found'}, 404
+        if semester.professor_id != current_user.id:
+            return {'message': 'Unauthorized'}, 403
+        db.session.delete(semester)
+        db.session.commit()
+        return {}, 204
+    
+class SemesterClasses(Resource):
+    def get(self, semester_id):
+        semester = db.session.get(Semester, semester_id)
+        if not semester:
+            return {'message': 'Semester not found'}, 404
+        classes = db.session.query(Class).filter_by(semester_id=semester_id).all()
+        classes_data = [cls.to_dict(rules=('-semester.classes','-registrations.course',)) for cls in classes]
+        return make_response(classes_data, 200)
 
 api.add_resource(CheckSession, '/check_session')
 api.add_resource(Login, '/login')
 api.add_resource(CurrentUser, '/current_user')
 api.add_resource(Professors, '/professors')
+api.add_resource(Logout, '/logout')
+api.add_resource(Semesters, '/semesters')
+api.add_resource(SemesterClasses, '/semesters/<int:semester_id>/classes')
 
 
 if __name__ == '__main__':
