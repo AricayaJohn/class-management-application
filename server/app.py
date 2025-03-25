21ae99baa474d82a1d3235a0e04559c05cc97208
John <jaricaya@gmail.com>
2025-03-25 22:52:23 +0800

    added professor and current user api

--- server/app.py
+++ server/app.py
@@ -44,9 +44,32 @@ class Login(Resource):
         
         return {'message': 'Invalid credentials'}, 401
 
+class Professors(Resource):
+    def post(self):
+        json = request.get_json()
+        try: 
+            new_professor = Professor(
+                username=json['username'],
+                name=json['name'],
+                department=json['department'],
+                office_location=json.get('office_location'),
+            )
+            new_professor.password_hash = json['password']
+            db.session.add(new_professor)
+            db.session.commit()
+            login_user(new_professor, remember=True)
+            return make_response(new_professor.to_dict(rules=('-_passowrd_hash', '-semesters.professor',)), 201)
+        except IntegrityError:
+            db.session.rollback()
+            return{'errors': 'Username already exists'}, 400
+        except Exception as e:
+            return {'errors': str(e)}, 500
+
 
 api.add_resource(CheckSession, '/check_session')
 api.add_resource(Login, '/login')
+api.add_resource(CurrentUser, '/current_user')
+api.add_resource(Professors, '/professors')
 
 
 if __name__ == '__main__':
