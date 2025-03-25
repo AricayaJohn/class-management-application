1c502d485b1a4573526d6bb266704103293a0065
John <jaricaya@gmail.com>
2025-03-25 21:12:22 +0800

    added Current user in api

--- server/app.py
+++ server/app.py
@@ -10,13 +10,43 @@ from models import Professor, Student, Semester, Class, Registration
 
 @login_manager.user_loader
 def load_user(professor_id):
-    return db.session.get()
+    return db.session.get(Professor, int(professor_id))
 
 class CheckSession(Resource):
     def get(self):
         if current_user.is_authenticated:
             return current_user.to_dict(rules=('-_password_hash',)), 200
         return {'message': 'Not authenticated'}, 401
+
+class CurrentUser(Resource):
+    @login_required
+    def get(self):
+        if current_user.is_authenticated:
+            return current_user.to_dict(rules=('-password_hash', '-semesters.professor',)), 200
+            return {'message': 'Not authenticated'}, 401
+
+class Login(Resource):
+    def post(self):
+        json = request.get_json()
+        username = json.get('username')
+        password = json.get('password')
+
+        if not username:
+            return {'message': 'Username required!'}, 400
+        if not password:
+            return {'message': 'Password required'}, 400
+        
+        professor = db.session.query(Professor).filter_by(username=username).first()
+
+        if professor and professor.authenticate(password):
+            login_user(professor, remember=True)
+            return current_user.to_dict(rules=('-_password_hash', '-semester.professor',)), 201
+        
+        return {'message': 'Invalid credentials'}, 401
+
+
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Login, '/login')
 
 
 if __name__ == '__main__':
