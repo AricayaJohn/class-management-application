4076ad1b432224705571dd1c96c5b6685968b8ba
John <jaricaya@gmail.com>
2025-03-27 05:35:15 +0800

    smester handler useeffect

--- client/src/components/SemesterHandler.js
+++ client/src/components/SemesterHandler.js
@@ -4,3 +4,25 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { UserContext } from "./Context";
 
+function SemesterHandler() {
+    const navigate = useNavigate();
+    const { addSemester, deleteSemester, Semester } = useContext(UserContext);
+    const [semester, setSemesters] = useState([]);
+    
+    useEffect(() => {
+        setSemesters()
+            .then((data) => {
+                console.log("Fetched semesters:", data);
+                setSemesters(data);
+            })
+            .catch((error) => console.error("Error fetching semesters: ", error));
+    }, Semesters);
+
+    const initialValues = {
+        nameYear: "",
+    };
+
+    const validationSchema = Yup.object({
+        nameYear: Yup.string()
+    })
+}
