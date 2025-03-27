6d33e1f22a7c4394bf790d680e5bbf36b041f1a2
John <jaricaya@gmail.com>
2025-03-27 15:21:18 +0800

    added state

--- client/src/components/AddClassPage.js
+++ client/src/components/AddClassPage.js
@@ -3,3 +3,11 @@ import { useNavigate } from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { UseContext } from "/Context";
+
+function AddClass() {
+    const navigate = useNavigate();
+    const { Semesters, addClass } = useContext(UserContext);
+    const [ semesters, setSemesters ] = useState([]);
+    
+    
+}
